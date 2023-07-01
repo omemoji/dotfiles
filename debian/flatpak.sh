e184721e6deb1afed63624eceed740d4fcca2cb2
omemoji <68148226+omemoji@users.noreply.github.com>
2023-07-02 01:12:38 +0900

    fix flatpak.sh

--- debian/flatpak.sh
+++ debian/flatpak.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 #Flatpak
-yes | sudo apt install flatpak
+sudo apt install flatpak
 #add repositories 
-yes | flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-yes | flatpak update
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+flatpak update
 #Social
-yes | flatpak install flathub com.slack.Slack
-yes | flatpak install flathub com.discordapp.Discord
-yes | flatpak install flathub us.zoom.Zoom
+flatpak install flathub com.slack.Slack
+flatpak install flathub com.discordapp.Discord
+flatpak install flathub us.zoom.Zoom
 #Creativity
-yes | flatpak install flathub org.blender.Blender
-yes | flatpak install flathub org.ardour.Ardour
-yes | flatpak install flathub org.kde.krita
+flatpak install flathub org.blender.Blender
+flatpak install flathub org.ardour.Ardour
+flatpak install flathub org.kde.krita
 #Game
-yes | flatpak install flathub com.valvesoftware.Steam
-yes | flatpak install flathub com.obsproject.Studio
+flatpak install flathub com.valvesoftware.Steam
+flatpak install flathub com.obsproject.Studio

2c233ee7a9081ffc6c24846b90fee071f273b6c9
omemoji <68148226+omemoji@users.noreply.github.com>
2023-07-02 00:24:28 +0900

    update debian.sh

--- debian.sh
+++ debian.sh
@@ -13,7 +13,7 @@ yes | sudo apt install openjdk-17-jdk
 
 ##Sound
 yes | sudo apt install pipewire wireplumber
-yes | sudo apt install gstreamer1.0-pipewire libpipewire-0.3-{0,dev,modules,modules-x11} libspa-0.2-{bluetooth,dev,jack,modules,libcamera} pipewire{,-{audio-client-libraries,pulse,bin,jack,alsa,v4l2,libcamera,tests}} \
+yes | sudo apt install gstreamer1.0-pipewire libpipewire-0.3-{0,dev,modules,modules-x11} libspa-0.2-{bluetooth,dev,jack,modules} pipewire{,-{audio-client-libraries,pulse,bin,jack,alsa,v4l2,libcamera,tests}} \
 wireplumber{,-doc} gir1.2-wp-0.4 libwireplumber-0.4-{0,dev} 
 yes | sudo apt install --no-install-recommends pavucontrol pulseaudio-utils
 

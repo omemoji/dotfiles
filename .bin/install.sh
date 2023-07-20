c450d1c3d733cf614ea1fe9d27ecfc2ffcd816b9
omemoji <68148226+omemoji@users.noreply.github.com>
2023-07-20 17:58:20 +0900

    fix

--- .bin/install.sh
+++ .bin/install.sh
@@ -11,7 +11,7 @@ fi
 
 if [ "$(uname -s)" = "Linux" ]; then
 
-    if [ "$(cat /etc/os-release | grep ^ID | sed -e "s/ID=//g")"="debian" ]; then
+    if [ "$(cat /etc/os-release | grep ^ID | sed -e "s/ID=//g")" = "debian" ]; then
         echo "Debian"
         ./.bin/apt.sh
     elif [ "$(cat /etc/os-release | grep ^ID | sed -e "s/ID=//g")" = "void" ]; then

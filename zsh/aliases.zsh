bb55ec01628bc7468d97abf1a8cb67cfc066af01
omemoji <me@omemoji.com>
2024-11-28 03:39:38 +0900

    [update]zsh/aliases.zsh

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,5 +1,5 @@
 alias gmount='google-drive-ocamlfuse ~/googledrive'
-alias startqemu="sudo qemu-system-x86_64 -enable-kvm -m 4096 -smp 8 -boot order=d -vga virtio"
+alias startqemu="qemu-system-x86_64 -enable-kvm -m 4096 -smp 8 -boot order=d -vga virtio"
 alias ranger='. ranger'
 alias tetris='vitetris'
 alias yt-mp4="yt-dlp -f 'bv*[ext=mp4]+ba[ext=m4a]/b[ext=mp4]'"

de48ae9aca74001ba1a342e6beb545ed67df60ab
omemoji <me@omemoji.com>
2025-06-18 01:27:54 +0900

    [update]xbps.sh

--- scripts/xbps.sh
+++ scripts/xbps.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 set -eu
 echo "Install repositories"
-
-yes | echo repository=https://raw.githubusercontent.com/Makrennel/hyprland-void/repository-x86_64-glibc | sudo tee /etc/xbps.d/hyprland-void.conf
-
 sudo xbps-install -Sy void-repo-multilib void-repo-multilib-nonfree void-repo-nonfree
+echo repository=https://raw.githubusercontent.com/Makrennel/hyprland-void/repository-x86_64-glibc | sudo tee /etc/xbps.d/hyprland-void.conf
 echo "Update"
 sudo xbps-install -Syu
 echo "### Install Void Linux apps ###"
